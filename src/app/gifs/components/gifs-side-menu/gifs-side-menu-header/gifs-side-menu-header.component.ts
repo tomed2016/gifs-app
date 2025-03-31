bc3bdabbc1bc043ddcda26729e14e5fdfcb2c505
Tomas Gonzalez Valdes <tomas.gonzalez.valdes@gmail.com>
2025-03-31 12:24:34 -0300

    side menu

---
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-header/gifs-side-menu-header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-side-menu-header',
+  imports: [],
+  templateUrl: './gifs-side-menu-header.component.html',
+})
+export default class GifsSideMenuHeaderComponent { }
---
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -0,0 +1,8 @@
+import {  Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  imports: [],
+  templateUrl: './gifs-side-menu-options.component.html',
+})
+export class GifsSideMenuOptionsComponent { }
