2ec9d8e44376da2ebbde0de29aa23db19c55e833
Tomas Gonzalez Valdes <tomas.gonzalez.valdes@gmail.com>
2025-04-01 15:14:07 -0300

    fin section componentes angular

--- src/app/gifs/components/gif-list/gif-list-item/gif-list-item.component.ts
+++ src/app/gifs/components/gif-list/gif-list-item/gif-list-item.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'gif-list-item',
   imports: [],
   templateUrl: './gif-list-item.component.html',
 })
-export class GifListItemComponent { }
+export class GifListItemComponent {
+  imageUrl = input.required<string>();
+}
---
+++ src/app/gifs/interfaces/image-url.ts
@@ -0,0 +1,5 @@
+export interface ImageUrl {
+  small: string;
+  medium: string;
+  large: string;
+}
